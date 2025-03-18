4302ea28c87e23ad833e493b789633313de0fc5b
AryanAD <aryanhohai@gmail.com>
2025-03-18 22:02:46 +0545

    setup

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+const App = () => {
+  return (
+    <>
+      <div className="font-bold font-circular-web text-blue-50">App</div>
+      <div
+        className="font-bold font-robert-medium text-blue-75
+      "
+      >
+        App
+      </div>
+      <div className="font-bold font-general text-blue-100">App</div>
+      <div className="font-bold font-robert text-blue-200">App</div>
+      <div className="font-bold font-zentry text-blue-300">App</div>
+      <div className="font-bold text-violet-300">App</div>
+      <div className="font-bold text-yellow-100">App</div>
+      <div className="font-bold text-yellow-300">App</div>
+    </>
+  );
+};
+
+export default App;
