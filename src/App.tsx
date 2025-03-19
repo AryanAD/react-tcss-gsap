acc67e7cac607227f8f89a8a416988c2e44c0c70
AryanAD <aryanhohai@gmail.com>
2025-03-19 22:20:27 +0545

    css and hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
+import Hero from "./components/Hero";
+
 const App = () => {
   return (
-    <>
-      <div className="font-bold font-circular-web text-blue-50">App</div>
-      <div
-        className="font-bold font-robert-medium text-blue-75
-      "
-      >
-        App
-      </div>
-      <div className="font-bold font-general text-blue-100">App</div>
-      <div className="font-bold font-robert text-blue-200">App</div>
-      <div className="font-bold font-zentry text-blue-300">App</div>
-      <div className="font-bold text-violet-300">App</div>
-      <div className="font-bold text-yellow-100">App</div>
-      <div className="font-bold text-yellow-300">App</div>
-    </>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero />
+
+      <section className="z-0 min-h-screen bg-blue-500" />
+    </main>
   );
 };
 
